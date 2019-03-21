e96c457222f3de73215fd3783952df0927dc459c
Sunny Cheung <sunnycyk@yahoo.com>
2019-03-21 16:13:10 +0800

    Keep alive subprovider (#2)
    * add keep-alive subprovider
    
    * fix error StatusCode
    
    rebuilt

--- src/subproviders/kl_rpc_subprovider.ts
+++ src/subproviders/kl_rpc_subprovider.ts
@@ -1,8 +1,7 @@
 import { StatusCodes } from '@0xproject/types';
-import { fetchAsync } from '@0xproject/utils';
 import { JSONRPCRequestPayload } from 'ethereum-types';
 import JsonRpcError = require('json-rpc-error');
-
+import RP = require('request-promise-native')
 import { Callback, ErrorCallback } from '../types';
 import { utils } from '../utils';
 
@@ -12,7 +11,7 @@ import { Subprovider } from './subprovider';
  * This class implements the [web3-provider-engine](https://github.com/MetaMask/provider-engine) subprovider interface.
  * It forwards on JSON RPC requests to the supplied `rpcUrl` endpoint
  */
-export class RPCSubprovider extends Subprovider {
+export class KeepAliveRPCSubprovider extends Subprovider {
     private readonly _rpcUrl: string;
     private readonly _requestTimeoutMs: number;
     constructor(rpcUrl: string, requestTimeoutMs: number = 20000) {
@@ -24,7 +23,7 @@ export class RPCSubprovider extends Subprovider {
     }
     /**
      * This method conforms to the web3-provider-engine interface.
-     * It is called internally by the ProviderEngine when it is this subproviders
+     * It is called internally byover the ProviderEngine when it is this subproviders
      * turn to handle a JSON RPC request.
      * @param payload JSON RPC payload
      * @param next Callback to call if this subprovider decides not to handle the request
@@ -33,44 +32,37 @@ export class RPCSubprovider extends Subprovider {
     // tslint:disable-next-line:prefer-function-over-method async-suffix
     public async handleRequest(payload: JSONRPCRequestPayload, _next: Callback, end: ErrorCallback): Promise<void> {
         const finalPayload = Subprovider._createFinalPayload(payload);
-        const headers = new Headers({
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        });
 
-        let response;
+        const headers = {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        };
+
+        let text;
         try {
-            response = await fetchAsync(
-                this._rpcUrl,
-                {
-                    method: 'POST',
-                    headers,
-                    body: JSON.stringify(finalPayload)
-                },
-                this._requestTimeoutMs,
-            );
-        } catch (err) {
-            end(new JsonRpcError.InternalError(err));
-            return;
-        }
-
-        const text = await response.text();
-        if (!response.ok) {
-            const statusCode = response.status;
-            switch (statusCode) {
-                case StatusCodes.MethodNotAllowed:
-                    end(new JsonRpcError.MethodNotFound());
-                    return;
-                case StatusCodes.GatewayTimeout:
-                    const errMsg =
-                        'Gateway timeout. The request took too long to process. This can happen when querying logs over too wide a block range.';
-                    const err = new Error(errMsg);
-                    end(new JsonRpcError.InternalError(err));
-                    return;
-                default:
-                    end(new JsonRpcError.InternalError(text));
-                    return;
-            }
+            text = await RP({
+              method:'POST',
+              body: JSON.stringify(finalPayload),
+              url: this._rpcUrl,
+              forever: true,
+              headers: headers
+            });
+        } catch (error) {
+          const statusCode = error.statusCode;
+          switch (statusCode) {
+              case StatusCodes.MethodNotAllowed:
+                  end(new JsonRpcError.MethodNotFound());
+                  return;
+              case StatusCodes.GatewayTimeout:
+                  const errMsg =
+                      'Gateway timeout. The request took too long to process. This can happen when querying logs over too wide a block range.';
+                  const err = new Error(errMsg);
+                  end(new JsonRpcError.InternalError(err));
+                  return;
+              default:
+                  end(new JsonRpcError.InternalError(error));
+                  return;
+          }
         }
 
         let data;
